50d47a94bd046ed75ef8d87d2f64040268566e51
haitang357 <haitang357@qq.com>
2025-06-24 06:25:35 +0800

    Modify RepeatCharHandler

--- src/main/java/com/codetest/candycrushletter/util/RepeatCharHandler.java
+++ src/main/java/com/codetest/candycrushletter/util/RepeatCharHandler.java
@@ -2,13 +2,14 @@ package com.codetest.candycrushletter.util;
 
 import org.springframework.stereotype.Component;
 
+import java.util.function.BiFunction;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Component
 public class RepeatCharHandler {
 
-    public static void removeRepeatChar(String input) {
+    private static void replaceRepeatChar(String input, BiFunction<String, String, String> function) {
         System.out.println("Input: " + input);
 
         String regex = "([a-zA-Z])\\1{2,}";
@@ -18,36 +19,42 @@ public class RepeatCharHandler {
 
         System.out.println("Output:");
         while (matcher.find()) {
-            input = input.replace(matcher.group(), "");
-            System.out.println("-> " + input);
+            String repeatString = matcher.group();
+
+            input = function.apply(input, repeatString);
+
             matcher = pattern.matcher(input);
         }
     }
 
+    public static void removeRepeatChar(String input) {
+        BiFunction<String, String, String> function = (inputString, repeatString) -> {
+            inputString = inputString.replace(repeatString, "");
+            System.out.println("-> " + inputString);
+            return inputString;
+        };
+
+        replaceRepeatChar(input, function);
+    }
+
     public static void replaceRepeatCharWithPreviousChar(String input) {
-        System.out.println("Input: " + input);
-
-        String regex = "([a-zA-Z])\\1{2,}";
-
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
-
-        System.out.println("Output:");
-        while (matcher.find()) {
-            String repeatString= matcher.group();
+        BiFunction<String, String, String> function = (inputString, repeatString) -> {
             char repeatChar = repeatString.charAt(0);
 
-            if(repeatChar!='a' && repeatChar!='A') {
+            if (repeatChar != 'a' && repeatChar != 'A') {
                 int repeatAscii = (int) repeatChar;
                 char previousChar = (char) (repeatAscii - 1);
-                input = input.replace(repeatString, Character.toString(previousChar));
-                System.out.println("-> " + input+", " + repeatString + " is replaced by " + previousChar);
+                inputString = inputString.replace(repeatString, Character.toString(previousChar));
+                System.out.println("-> " + inputString + ", " + repeatString + " is replaced by " + previousChar);
             } else {
-                input = input.replace(repeatString, "");
-                System.out.println("-> " + input);
+                inputString = inputString.replace(repeatString, "");
+                System.out.println("-> " + inputString);
             }
-            matcher = pattern.matcher(input);
-        }
+
+            return inputString;
+        };
+
+        replaceRepeatChar(input, function);
     }
 
 }
